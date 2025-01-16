66c8ffaf052d3803577a27ef147bfa18bf9994e4
BrianHKKR <BrianHKKR@users.noreply.github.com>
2025-01-16 14:40:14 +0900

    first commit

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <h1 className="text-4xl font-bold">안녕하세요!</h1>
+        <p className="mt-4">제 첫 웹사이트입니다 😊</p>
+      </main>
+    )
+  }
